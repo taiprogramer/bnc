89bd6702f187877389e422718ebb54b7ec72ef7e
taiprogramer <taiprogramer@protonmail.com>
2020-07-09 20:16:16 +0700

    [add bnc.h]
    - macros for datatypes & entry point.

---
+++ src/bnc.h
@@ -0,0 +1,29 @@
+#ifndef BNC_H
+#define BNC_H
+
+// [entry macro replace main function]
+#define ENTRY int main(int argc, char* argv[]) 
+
+// [datatypes]
+// char
+typedef char charvar;
+typedef char* stringvar;
+
+// int
+typedef charvar int8var;
+typedef unsigned char uint8var;
+typedef short int16var;
+typedef unsigned short uint16var;
+typedef int int32var;
+typedef unsigned int uint32var;
+typedef long long int int64var;
+typedef unsigned long long int uint64var;
+typedef int32var intvar;
+
+// real
+typedef float real32var;
+typedef double real64var;
+typedef real64var realvar;
+
+#endif
+
