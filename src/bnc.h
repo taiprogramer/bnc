f4efde5a635ed88c6d348d23e7da94996a7da8d9
taiprogramer <me@taiprogramer.xyz>
2020-07-10 12:54:28 +0700

    [stdin: Keyboard]
    - Add waitForString function.
    - Auto clean-up memory.

--- src/bnc.h
+++ src/bnc.h
@@ -1,9 +1,39 @@
 #ifndef BNC_H
 #define BNC_H
 
+/**
+ * Preprocessor magic to make initializers work somewhat portably
+ * Modified from
+ * http://stackoverflow.com/questions/1113409/attribute-constructor-equivalent-in-vc
+ * I copy from cs50: https://github.com/cs50/libcs50/blob/develop/src/cs50.c
+ */
+#if defined(_MSC_VER) // MSVC
+#pragma section(".CRT$XCU", read)
+#define INITIALIZER_(FUNC, PREFIX)                                             \
+    static void FUNC(void);                                                    \
+    __declspec(allocate(".CRT$XCU")) void (*FUNC##_)(void) = FUNC;             \
+    __pragma(comment(linker, "/include:" PREFIX #FUNC "_")) static void FUNC(  \
+        void)
+#ifdef _WIN64
+#define INITIALIZER(FUNC) INITIALIZER_(FUNC, "")
+#else
+#define INITIALIZER(FUNC) INITIALIZER_(FUNC, "_")
+#endif
+#elif defined(__GNUC__) // GCC, Clang, MinGW
+#define INITIALIZER(FUNC)                                                      \
+    static void FUNC(void) __attribute__((constructor));                       \
+    static void FUNC(void)
+#else
+#error The CS50 library requires some compiler-specific features, \
+           but we do not recognize this compiler/version. Please file an issue at \
+           https://github.com/cs50/libcs50
+#endif
+
 // [entry macro replace main function]
-#define ENTRY int main(int argc, char* argv[]){
-#define EXIT return 0; }
+#define ENTRY int main(int argc, char* argv[]) {
+#define EXIT                                                                   \
+    return 0;                                                                  \
+    }
 
 // [datatypes]
 // char
@@ -37,6 +67,11 @@ struct Screen {
     struct Screen (*r)(real64var);
     struct Screen (*s)(stringvar);
 } Screen;
+
+// [stdin: Keyboard]
+struct Keyboard {
+    stringvar (*waitForString)(uint8var max_length);
+} Keyboard;
 
 #endif
 
--- src/libbnc.c
+++ src/libbnc.c
@@ -1,5 +1,22 @@
-#include <stdio.h>
 #include "bnc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static stringvar ARRAY strings = NULL;
+static uint64var number_of_strings_that_have_been_allocated = 0;
+
+static void clean_up() {
+    for (int i = 0; i < number_of_strings_that_have_been_allocated; ++i) {
+        free(strings[i]);
+    }
+    free(strings);
+}
+
+/**
+ * Called automatically before execution enters main.
+ */
+INITIALIZER(setup) { atexit(clean_up); }
 
 // [stdout: Screen]
 struct Screen screen_show(stringvar message) {
@@ -35,4 +52,51 @@ struct Screen screen_s(stringvar message) {
 struct Screen Screen = {&screen_show,    &screen_i, &screen_c,
                         &screen_newline, &screen_r, &screen_s};
 
+// [stdin: Keyboard]
+stringvar keyboard_wait_for_string(uint8var max_length) {
+    stringvar s = (stringvar)malloc(sizeof(charvar) * (max_length + 1));
+
+    if (s == NULL) {
+        return NULL;
+    }
+
+    fgets(s, max_length + 1, stdin);
+
+    uint8var len = (uint8var)strlen(s);
+    if (s[len - 1] == '\r' || s[len - 1] == '\n' || s[len - 1] == EOF) {
+        s[len - 1] = '\0';
+    }
+
+    stringvar temp = s;
+    s = (stringvar)malloc(sizeof(charvar) * (strlen(s) + 1));
+
+    if (s == NULL) {
+        free(temp);
+        return NULL;
+    }
+
+    strcpy(s, temp);
+
+    // add $s to $strings for auto free after used
+    if (strings == NULL) {
+        strings = (stringvar ARRAY)malloc(sizeof(stringvar));
+    } else {
+        strings = (stringvar ARRAY)realloc(
+            strings, number_of_strings_that_have_been_allocated + 1);
+    }
+
+    if (strings == NULL) {
+        free(s);
+        free(temp);
+        return NULL;
+    }
+
+    free(temp);
+    strings[number_of_strings_that_have_been_allocated] = s;
+    number_of_strings_that_have_been_allocated++;
+
+    return s;
+}
+
+struct Keyboard Keyboard = {&keyboard_wait_for_string};
 
